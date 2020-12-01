74de29f002adbdfc112919244b4800e44974151b
RafaelIrineu <rafaelirineu01@gmail.com>
2020-12-01 02:42:02 -0300

    Inserido Navigation e corrigindo layouts

--- app/src/main/java/com/rafaelirineu/desafiowebservices/login/LoginFragment.kt
+++ app/src/main/java/com/rafaelirineu/desafiowebservices/login/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 import com.rafaelirineu.desafiowebservices.R
 
 class LoginFragment : Fragment() {
@@ -14,5 +16,15 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        view.findViewById<Button>(R.id.btnCreate_fragmentLogin).setOnClickListener {
+            navController.navigate(R.id.action_loginFragment_to_registerFragment)
+        }
     }
 }

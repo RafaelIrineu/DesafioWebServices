a718a8507fcba07e3d511faaabbe474ceb248823
RafaelIrineu <rafaelirineu01@gmail.com>
2020-12-03 14:50:48 -0300

    Navigation dos novos fragments

--- app/src/main/java/com/rafaelirineu/desafiowebservices/view/ComicCoverFragment.kt
+++ app/src/main/java/com/rafaelirineu/desafiowebservices/view/ComicCoverFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.Navigation
 import com.rafaelirineu.desafiowebservices.R
 
 class ComicCoverFragment : Fragment() {
@@ -14,5 +16,15 @@ class ComicCoverFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_comic_cover, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        view.findViewById<ImageView>(R.id.imgClose_fragmentComicCover).setOnClickListener {
+            navController.navigate(R.id.action_comicCoverFragment_to_comicInfoFragment)
+        }
     }
 }
--- app/src/main/java/com/rafaelirineu/desafiowebservices/view/ComicInfoFragment.kt
+++ app/src/main/java/com/rafaelirineu/desafiowebservices/view/ComicInfoFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.Navigation
 import com.rafaelirineu.desafiowebservices.R
 import com.rafaelirineu.desafiowebservices.viewmodel.ComicViewModel
 
 class ComicInfoFragment : Fragment() {
 
-    private lateinit var _viewModel : ComicViewModel
+    private lateinit var _viewModel: ComicViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +23,11 @@ class ComicInfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
 
+        val navController = Navigation.findNavController(view)
+
+        view.findViewById<ImageView>(R.id.imgBack_fragmentComicInfo).setOnClickListener {
+            navController.navigate(R.id.action_comicInfoFragment_to_homeFragment)
+        }
+    }
 }
